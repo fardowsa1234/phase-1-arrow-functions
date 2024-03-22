6c7ed73536f6ea492de7ed41a35ec97d36571b47
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-03-22 08:46:40 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression called divide that divides two numbers
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+
+// Arrow function called square that takes one parameter and returns its square
+const square = (num) => num * num;
+
+// Arrow function called add that takes two parameters and returns their sum
+const add = (num1, num2) => num1 + num2;
